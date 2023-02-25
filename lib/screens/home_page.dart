f158c6c0eb3e475d1e05ddb42988201c82f2020a
Jeremy John <jjseah00@gmail.com>
2023-02-25 16:15:05 +0800

    built project

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bak/theme/colors/light_colors.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+import 'package:flutter_bak/widgets/task_column.dart';
+import 'package:flutter_bak/widgets/active_project_card.dart';
+import 'package:flutter_bak/widgets/top_container.dart';
+
+class HomePage extends StatelessWidget {
+  Text subheading(String title) {
+    return Text(
+      title,
+      style: TextStyle(
+          color: LightColors.kDarkBlue,
+          fontSize: 20.0,
+          fontWeight: FontWeight.w700,
+          letterSpacing: 1.2),
+    );
+  }
+
+  static CircleAvatar calendarIcon() {
+    return CircleAvatar(
+      radius: 25.0,
+      backgroundColor: LightColors.kGreen,
+      child: Icon(
+        Icons.calendar_today,
+        size: 20.0,
+        color: Colors.white,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: LightColors.kLightYellow,
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            TopContainer(
+              height: 200,
+              width: width,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Icon(Icons.menu,
+                            color: LightColors.kDarkBlue, size: 30.0),
+                        Icon(Icons.search,
+                            color: LightColors.kDarkBlue, size: 25.0),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 0, vertical: 0.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          CircularPercentIndicator(
+                            radius: 90.0,
+                            lineWidth: 5.0,
+                            animation: true,
+                            percent: 0.75,
+                            circularStrokeCap: CircularStrokeCap.round,
+                            progressColor: LightColors.kRed,
+                            backgroundColor: LightColors.kDarkYellow,
+                            center: CircleAvatar(
+                              backgroundColor: LightColors.kBlue,
+                              radius: 35.0,
+                              backgroundImage: AssetImage(
+                                'assets/images/avatar.png',
+                              ),
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              Container(
+                                child: Text(
+                                  'Jeremy Smith',
+                                  textAlign: TextAlign.start,
+                                  style: TextStyle(
+                                    fontSize: 22.0,
+                                    color: LightColors.kDarkBlue,
+                                    fontWeight: FontWeight.w800,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                child: Text(
+                                  'Delivery Driver',
+                                  textAlign: TextAlign.start,
+                                  style: TextStyle(
+                                    fontSize: 16.0,
+                                    color: Colors.black45,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    )
+                  ]),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      color: Colors.transparent,
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 20.0, vertical: 10.0),
+                      child: Column(
+                        children: <Widget>[
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              subheading('My Tasks'),
+                            ],
+                          ),
+                          SizedBox(height: 15.0),
+                          TaskColumn(
+                            icon: Icons.alarm,
+                            iconBackgroundColor: LightColors.kRed,
+                            title: 'To Do',
+                            subtitle: '5 tasks now. 1 started',
+                          ),
+                          SizedBox(
+                            height: 15.0,
+                          ),
+                          TaskColumn(
+                            icon: Icons.blur_circular,
+                            iconBackgroundColor: LightColors.kDarkYellow,
+                            title: 'In Progress',
+                            subtitle: '1 tasks now. 1 started',
+                          ),
+                          SizedBox(height: 15.0),
+                          TaskColumn(
+                            icon: Icons.check_circle_outline,
+                            iconBackgroundColor: LightColors.kBlue,
+                            title: 'Done',
+                            subtitle: '18 tasks now. 13 started',
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
